4b8ba2a8dcd3b84022589100e33db64bd1b7a966
RohitShetty <rohitshetty@credenceanalytics.com>
2023-11-12 14:45:14 +0530

    added mongoose model and new document

--- natours/server.js
+++ natours/server.js
@@ -19,6 +19,38 @@ const statuuss = mongoose
     console.log('status Conncted!!');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+const testTours = new Tour({
+  name: 'ROLLS',
+  rating: 4.5,
+  price: 45000,
+});
+
+testTours
+  .save()
+  .then((doc) => {
+    console.log('doc' + doc);
+  })
+  .catch((err) => {
+    console.log('ERRR' + err);
+  });
+
 console.log('resp-->' + JSON.stringify(statuuss));
 
 app.listen(port, () => {
